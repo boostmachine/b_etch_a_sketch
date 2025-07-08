639f1d31cdf4cd15fce08e391adaf938bbdf3c56
boostmachine <184125960+boostmachine@users.noreply.github.com>
2025-07-08 16:47:34 +0300

    Add js hover

--- script.js
+++ script.js
@@ -3,9 +3,12 @@
 // DOM elements
 const container = document.querySelector(".container");
 
-for(let i = 1; i < 17; i++) {
-  let div = document.createElement("div")
+for(let i = 1; i < 257; i++) {
+  let div = document.createElement("div");
   div.textContent = `div #${i}`;
-  div.className = "box"
+  div.className = "box";
+  div.addEventListener("mouseover", () => {
+    div.style.backgroundColor = `black`;
+  });
   container.appendChild(div);
 }
